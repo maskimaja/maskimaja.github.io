5757e4c2942eaa29fc7975a410ec150bff9415f5
maskimaja <99765959+erlkim@users.noreply.github.com>
2024-11-04 21:40:11 +0700

    Site updated: 2024-11-04 21:40:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/04/test/","2024/11/04/puisi/","2024/11/04/hello-world/"];function toRandomPost(){
+var posts=["2024/11/04/sampah/","2024/11/04/test/","2024/11/04/puisi/","2024/11/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
